57ddada5e26ec1e26e8c940c597edce61e60ae8b
Rafael.Alencar <rafaelesdras@gmail.com>
2016-06-29 19:50:00 -0300

    Correção de configuração e nome de testes de integração das Classes Peticionáveis.

--- src/test/java/br/jus/stf/autuacao/peticionamento/ConsultasClassePeticionavelIntegrationTests.java
+++ src/test/java/br/jus/stf/autuacao/peticionamento/ConsultasClassePeticionavelIntegrationTests.java
@@ -5,9 +5,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 
 import  br.jus.stf.core.framework.testing.IntegrationTestsSupport;
 
@@ -18,13 +18,14 @@ import  br.jus.stf.core.framework.testing.IntegrationTestsSupport;
  * @since 09/05/2016
  *
  */
-@Ignore
 @SpringApplicationConfiguration(ApplicationContextInitializer.class)
-public class ClassePeticionalIntegrationTests extends IntegrationTestsSupport {
+@WebIntegrationTest({"server.port:0", "eureka.client.enabled:false"})
+public class ConsultasClassePeticionavelIntegrationTests extends IntegrationTestsSupport {
 	
 	@Test
 	public void listarClassesPeticionaveis() throws Exception{
-		mockMvc.perform(get("/api/classes-peticionaveis")).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(26)));
+		mockMvc.perform(get("/api/classes-peticionaveis")).andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(26)));
 	}
 
 }
