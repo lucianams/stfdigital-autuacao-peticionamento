b786bf314d70db0db7a7ede40504beeb8d4a14a9
Rodrigo Barreiros <barreiros.rodrigo@gmail.com>
2016-07-06 16:54:42 -0300

    Melhorias na configuração de segurança

--- src/main/java/br/jus/stf/autuacao/peticionamento/infra/configuration/FeignClientsConfigurer.java
+++ src/main/java/br/jus/stf/autuacao/peticionamento/infra/configuration/FeignClientsConfigurer.java
@@ -1,16 +1,11 @@
 package br.jus.stf.autuacao.peticionamento.infra.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 import feign.RequestInterceptor;
 
@@ -20,31 +15,13 @@ import feign.RequestInterceptor;
  * @since 1.0.0
  * @since 30.06.2016
  */
+@EnableOAuth2Client
 @Configuration
 public class FeignClientsConfigurer {
 
-	@Autowired
-	private ResourceServerProperties sso;
-	
 	@Bean
-	@ConfigurationProperties(prefix = "security.oauth2.client")
-	public ClientCredentialsResourceDetails clientCredentialsResourceDetails() {
-		return new ClientCredentialsResourceDetails();
-	}
-
-	@Bean
-	public RequestInterceptor oauth2FeignRequestInterceptor() {
-		return new OAuth2FeignRequestInterceptor(new DefaultOAuth2ClientContext(), clientCredentialsResourceDetails());
-	}
-
-	@Bean
-	public ResourceServerTokenServices tokenServices() {
-		return new UserInfoTokenServices(sso.getUserInfoUri(), sso.getClientId());
-	}
-
-	@Bean
-	public OAuth2RestTemplate clientCredentialsRestTemplate() {
-		return new OAuth2RestTemplate(clientCredentialsResourceDetails());
+	public RequestInterceptor feignRequestInterceptor(OAuth2ClientContext oAuth2ClientContext) {
+		return new OAuth2FeignRequestInterceptor(oAuth2ClientContext, new ClientCredentialsResourceDetails());
 	}
 
 }
