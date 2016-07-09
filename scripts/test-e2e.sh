99e34ab9a059748498f30b6c300884e3ff944833
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-09 17:09:34 -0300

    Teste e2e no travis.

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-MAIN_DOCKER_COMPOSE_FILE="-f docker-compose.yml"
-COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f docker-compose.e2e.yml"
+MAIN_DOCKER_COMPOSE_FILE="-f docker-compose${1:-}.yml"
+COMPOSE_FILES_PARAMS="$MAIN_DOCKER_COMPOSE_FILE -f compose/docker-compose.e2e.yml"
 
 docker-compose $COMPOSE_FILES_PARAMS up -d
 
 ./scripts/wait-up.sh "https://docker:8765/peticionamento/info" 600
 ./scripts/wait-up.sh "https://docker:8765/documents/info" 30
-./scripts/wait-up.sh "https://docker/OnlineEditorsExample/" 30
 ./scripts/wait-up.sh "http://docker:4444/wd/hub" 30
 
 gradle gulpTestE2E
