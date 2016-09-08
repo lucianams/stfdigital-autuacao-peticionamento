fad53522b16859211707bc473a66e4346c713d29
Rafael.Alencar <rafaelesdras@gmail.com>
2016-09-08 15:58:34 -0300

    Adequação dos scripts à sintaxe Oracle.

--- src/main/resources/db/migration/V1_1__Novos_conceitos.sql
+++ src/main/resources/db/migration/V1_1__Novos_conceitos.sql
@@ -11,12 +11,11 @@ alter table peticionamento.peticao_preferencia add constraint fk_peticao_pepr fo
 alter table peticionamento.peticao_preferencia add constraint fk_preferencia_pepr foreign key (seq_preferencia) references peticionamento.preferencia(seq_preferencia);
 
 drop table peticionamento.peticao_envolvido;
-alter table peticionamento.envolvido add column tip_polo varchar2(11) not null;
+alter table peticionamento.envolvido add tip_polo varchar2(11) not null;
 alter table peticionamento.envolvido add constraint ck_envo_tip_polo check (tip_polo in ('ATIVO', 'PASSIVO', 'INTERESSADO'));
-alter table peticionamento.envolvido alter column nom_envolvido rename to nom_apresentacao;
-alter table peticionamento.envolvido alter column nom_apresentacao set not null;
-alter table peticionamento.envolvido add column seq_pessoa number;
-alter table peticionamento.envolvido add column seq_protocolo number not null;
+alter table peticionamento.envolvido rename column nom_envolvido to nom_apresentacao;
+alter table peticionamento.envolvido add seq_pessoa number;
+alter table peticionamento.envolvido add seq_protocolo number not null;
 alter table peticionamento.envolvido add constraint fk_peticao_envo foreign key (seq_protocolo) references peticionamento.peticao(seq_protocolo);
 
 create sequence peticionamento.seq_anexo increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
@@ -31,13 +30,13 @@ create table peticionamento.orgao_peticionador (seq_pessoa number not null, nom_
 
 create table peticionamento.associado (seq_associado number not null, seq_pessoa number not null, nom_pessoa varchar2(100) not null, tip_associado varchar2(13) not null, dsc_cargo_funcao varchar2(50), seq_pessoa_orgao number not null, constraint pk_associado primary key (seq_associado));
 alter table peticionamento.associado add constraint fk_orgao_peticionador_asso foreign key (seq_pessoa_orgao) references peticionamento.orgao_peticionador(seq_pessoa);
-alter table peticionamento.associado add constraint uk_asso_seq_pessoa unique key (seq_pessoa, seq_pessoa_orgao);
+alter table peticionamento.associado add constraint uk_asso_seq_pessoa unique (seq_pessoa, seq_pessoa_orgao);
 alter table peticionamento.associado add constraint ck_asso_tip_associado check (tip_associado in ('ASSOCIADO', 'GESTOR', 'REPRESENTANTE'));
 
-alter table peticionamento.peticao add column num_peticao number not null;
-alter table peticionamento.peticao add column num_ano integer not null;
-alter table peticionamento.peticao add column sig_peticionador varchar2(30) not null;
-alter table peticionamento.peticao add column dat_peticionamento date not null;
-alter table peticionamento.peticao add constraint uk_peti_num_peticao unique key (num_peticao, num_ano);
+alter table peticionamento.peticao add num_peticao number not null;
+alter table peticionamento.peticao add num_ano integer not null;
+alter table peticionamento.peticao add sig_peticionador varchar2(30) not null;
+alter table peticionamento.peticao add dat_peticionamento date not null;
+alter table peticionamento.peticao add constraint uk_peti_num_peticao unique (num_peticao, num_ano);
 alter table peticionamento.peticao add constraint fk_classe_peticionavel_peti foreign key (sig_classe) references peticionamento.classe_peticionavel(sig_classe);
 alter table peticionamento.peticao add constraint fk_orgao_peticionador_peti foreign key (seq_orgao) references peticionamento.orgao_peticionador(seq_pessoa);
