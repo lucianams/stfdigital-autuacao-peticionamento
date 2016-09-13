0412ea8c946258e077f9a7a70c53f8a0c4db45f7
Rafael.Alencar <rafaelesdras@gmail.com>
2016-09-13 16:10:48 -0300

    Ajustes na tabela de eventos.

--- src/main/resources/db/migration/V1__Versao_inicial.sql
+++ src/main/resources/db/migration/V1__Versao_inicial.sql
@@ -12,7 +12,7 @@ create table peticionamento.peticao_envolvido (seq_protocolo number not null, se
 alter table peticionamento.peticao_envolvido add constraint fk_peticao_peen foreign key (seq_protocolo) references peticionamento.peticao(seq_protocolo);
 alter table peticionamento.peticao_envolvido add constraint fk_envolvido_peen foreign key (seq_envolvido) references peticionamento.envolvido(seq_envolvido);
 
-create table peticionamento.evento (seq_evento number not null, nom_evento varchar2(50) not null, dat_criacao date not null, bin_detalhe clob not null, tip_status smallint not null, constraint pk_evento primary key (seq_evento));
+create table peticionamento.evento (seq_evento number not null, nom_evento varchar2(100) not null, dat_criacao date not null, bin_detalhe clob not null, tip_status smallint not null, constraint pk_evento primary key (seq_evento));
 
 create table peticionamento.peticao_evento (seq_protocolo number not null, seq_evento number not null, constraint pk_peticao_evento primary key (seq_protocolo, seq_evento));
 alter table peticionamento.peticao_evento add constraint fk_peticao_peev foreign key (seq_protocolo) references peticionamento.peticao(seq_protocolo);
