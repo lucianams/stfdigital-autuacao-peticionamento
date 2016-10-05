6f1430a23cb2e548714a693eb55c31f1699e4aee
Lucas.Rodrigues <lucas.rodrigues@stf.jus.br>
2016-10-05 19:24:10 -0300

    Renomeado o nome do serviço

--- scripts/wait-ready.sh
+++ scripts/wait-ready.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-./shared/scripts/wait-up.sh "https://docker:8765/services/manage/info" 300
+./shared/scripts/wait-up.sh "https://docker:8765/processos/manage/info" 300
 ./shared/scripts/wait-up.sh "https://docker:8765/peticionamento/manage/info" 300
 ./shared/scripts/wait-up.sh "https://docker:8765/documents/manage/info" 30
